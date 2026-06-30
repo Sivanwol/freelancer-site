f11bd6370dea267f90a21ee3ac070a736256951d
Sivan Wolberg <fastwings@gmail.com>
2026-07-01 02:11:12 +0300

    Update Soro blog embed ID
    Point the blog embed at the new TrySoro embed script URL.

--- components/blog/soro-embed.tsx
+++ components/blog/soro-embed.tsx
@@ -1,11 +1,11 @@
 import Script from 'next/script';
 
-const SORO_EMBED_SRC = 'https://app.trysoro.com/api/embed/0c8cac65-0917-4709-a7b9-7d0aced09806';
+const SORO_EMBED_SRC = 'https://app.trysoro.com/api/embed/cba1e92c-70c0-4f06-9f93-6fc454e7a2d0';
 
 export default function SoroEmbed() {
   return (
     <>
-      <div id="soro-blog" />
+      <div id="soro-blog"></div>
       <Script src={SORO_EMBED_SRC} strategy="afterInteractive" />
     </>
   );
