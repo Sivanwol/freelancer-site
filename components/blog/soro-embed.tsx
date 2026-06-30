e28ff4ac62b981ab27f448b6ba260ae34d07fac5
Sivan Wolberg <fastwings@gmail.com>
2026-07-01 02:34:50 +0300

    Fix Soro embed on client-side blog navigation
    Replace next/script with a deferred script injected on mount so the
    embed re-initializes when navigating to the blog without a full reload.

--- components/blog/soro-embed.tsx
+++ components/blog/soro-embed.tsx
@@ -1,12 +1,34 @@
-import Script from 'next/script';
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { usePathname } from '@/i18n/routing';
 
 const SORO_EMBED_SRC = 'https://app.trysoro.com/api/embed/cba1e92c-70c0-4f06-9f93-6fc454e7a2d0';
+const SORO_SCRIPT_ID = 'soro-blog-embed-script';
 
 export default function SoroEmbed() {
-  return (
-    <>
-      <div id="soro-blog"></div>
-      <Script src={SORO_EMBED_SRC} strategy="afterInteractive" />
-    </>
-  );
+  const containerRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    container.innerHTML = '';
+    document.getElementById(SORO_SCRIPT_ID)?.remove();
+
+    const script = document.createElement('script');
+    script.id = SORO_SCRIPT_ID;
+    script.src = SORO_EMBED_SRC;
+    script.defer = true;
+
+    container.insertAdjacentElement('afterend', script);
+
+    return () => {
+      script.remove();
+      container.innerHTML = '';
+    };
+  }, [pathname]);
+
+  return <div id="soro-blog" ref={containerRef} />;
 }
